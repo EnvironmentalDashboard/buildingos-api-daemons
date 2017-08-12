4db0f4ea45b2444f244ae90a6bc6e7f497a94ca6
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2017-08-12 11:24:00 -0500

    delete daemons that arent supposed to exist

--- restart.sh
+++ restart.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 source db.sh
-pop=2
+pop=2 # keep this number of buildingosd instances running
 while read -r line
 do
 	for pid in $(echo $line | tr " " "\n")
 	do
-		if ps -p $pid > /dev/null; then
+		if ps -p $pid > /dev/null; then # process is running
 			(( pop-- ))
-			# echo "$pid exists"
 		else # process is not running, but is in db
 			mysql -u $user -"p$pass" -"h$server" $name -BNse "DELETE FROM daemons WHERE pid = $pid" 2>/dev/null
-			# echo "$pid doesnt exist"
 		fi
-		if (( pop == 0 )); then
+		if (( pop == 0 )); then # more daemons exist than there should be
+			mysql -u $user -"p$pass" -"h$server" $name -BNse "DELETE FROM daemons WHERE pid < $pid" 2>/dev/null
 			exit 0
 		fi
 	done
-done <<< `mysql -u $user -"p$pass" -"h$server" $name -BNse "SELECT pid FROM daemons WHERE target_res = 'live'" 2>/dev/null`
+done <<< `mysql -u $user -"p$pass" -"h$server" $name -BNse "SELECT pid FROM daemons WHERE target_res = 'live' ORDER BY pid DESC" 2>/dev/null` # order by desc bc those processes were started more recently
 
-for ((i=0; i<pop; i++)); do
+for ((i=0; i<pop; i++)); do # keep pop number of daemons running
 	`/var/www/html/oberlin/daemons/buildingosd -d`
 done
