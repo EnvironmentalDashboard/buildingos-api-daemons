d0c5b347b3f9162c045793d870ffae1ec1a8b8b6
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-02-21 14:14:30 -0500

    this was actually never needed

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build -t buildingosd .
-docker run -dit -p 52001:3306 -v /root:/root --restart always --name buildingosd1 buildingosd
-docker run -dit -p 52002:3306 -v /root:/root --restart always --name buildingosd2 buildingosd
+docker run -dit -v /root:/root --restart always --name buildingosd1 buildingosd
+docker run -dit -v /root:/root --restart always --name buildingosd2 buildingosd
 # if using gdb, need to add `--cap-add=SYS_PTRACE --security-opt seccomp=unconfined` to docker run, see https://stackoverflow.com/a/46676907/2624391
